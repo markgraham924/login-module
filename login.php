48e029944c7d38ead1b4b01ddaa46c8a6525528d
markgraham924 <50046031+markgraham924@users.noreply.github.com>
2019-06-18 10:45:07 +0100

    Final version complete

--- login.php
+++ login.php
@@ -4,7 +4,39 @@
         $userInp = $_GET["userInp"];
         $passInp = $_GET["passInp"];
 
-        echo $passInpHash = hash('sha512', $passInp);
+
+        $servername = "160.153.131.192";
+        $username = "adminUser";
+        $password = "/someWHERE924";
+        $dbname = "users";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+
+        $sql = "SELECT userSto, passSto FROM infor";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $userStore = $row["userSto"];
+                if ($userStore = $userInp){
+                    $userSto = $row["userSto"];
+                    $passSto = $row["passSto"];
+                }
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+        $boolHash = password_verify($passInp, $passSto);
+        if ($boolHash = TRUE){
+            echo "correct login";
+        }
 
     ?>
 </html>
---
+++ signup.php
@@ -0,0 +1,33 @@
+<html>
+    <?php
+
+        $userInp = $_GET["userInp"];
+        $passInp = $_GET["passInp"];
+
+        $passInpHash = hash('sha512', $passInp);
+
+        $servername = "160.153.131.192";
+        $username = "adminUser";
+        $password = "/someWHERE924";
+        $dbname = "users";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "INSERT INTO infor (userSto, passSto)
+        VALUES ('$userInp', '$passInpHash')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+
+    ?>
+</html>
